acf4ff90637ffe254e478602c0ed4ab60f951ce4
Fatma Gaye LO <FGLO>
2021-03-25 14:28:43 +0000

    setting selenium

---
+++ vaanah/test.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+class Test(StaticLiveServerTestCase):
+
+  def testform(self):
+    selenium = webdriver.Chrome()
