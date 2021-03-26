e7b487839581b990ecd8f7310e44825d4d71c971
Fatma Gaye LO <FGLO>
2021-03-26 10:21:33 +0000

    setting python behave

--- vaanah/test.py
+++ vaanah/test.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+#from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 class Test(StaticLiveServerTestCase):
 
   def testform(self):
     selenium = webdriver.Chrome()
+    
