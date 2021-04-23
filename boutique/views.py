608ace30aae992f902532da94f8486b4a3f1d087
Fatma Gaye LO <FGLO>
2021-04-23 16:23:28 +0000

    display store informations

--- boutique/views.py
+++ boutique/views.py
@@ -1,6 +1,10 @@
 from django.views import generic
 from oscar.core.loading import get_model, get_classes
 from oscar.views import sort_queryset
+from django.urls import reverse, reverse_lazy
+from django.shortcuts import get_object_or_404, redirect
+
+Product = get_model('catalogue', 'Product')
 
 Partner = get_model('partner', 'Partner')
 (
@@ -11,6 +15,7 @@ Partner = get_model('partner', 'Partner')
     ['PartnerSearchForm', 'PartnerCreateForm', 'PartnerAddressForm',
      'NewUserForm', 'UserEmailForm', 'ExistingUserForm'])
 Boutique = get_model('boutique', 'Boutique')
+
 class BoutiqueListView(generic.ListView):
     model = Partner
     context_object_name = 'partners'
@@ -49,36 +54,43 @@ class BoutiqueListView(generic.ListView):
 
 
 class BoutiqueDetailView(generic.DetailView):
-    model = Partner
     template_name = 'boutique/boutique_details.html'
-    context_object_name = 'partners'
-    form_class = PartnerSearchForm
-    #success_url = reverse_lazy('dashboard:partner-list')
-    def get_queryset(self):
-        qs = self.model._default_manager.all()
-        qs = sort_queryset(qs, self.request, ['name'])
+    form_class = PartnerAddressForm
+    success_url = reverse_lazy('boutique:boutique-list')
+    def get_object(self, queryset=None):
+        self.partner = get_object_or_404(Partner, pk=self.kwargs['pk'])
+        address = self.partner.primary_address
+        patnerId = self.request.GET.get('pk')
+        print(patnerId)
 
-        self.description = ("All boutiques")
+        if address is None:
+            address = self.partner.addresses.model(partner=self.partner)
+        return address
 
-        # We track whether the queryset is filtered to determine whether we
-        # show the search form 'reset' button.
-        self.is_filtered = False
-        self.form = self.form_class(self.request.GET)
-        if not self.form.is_valid():
-            return qs
-
-        data = self.form.cleaned_data
-
-        if data['name']:
-            qs = qs.filter(name__icontains=data['name'])
-            self.description = _("Partners matching '%s'") % data['name']
-            self.is_filtered = True
-
-        return qs
+    def get_initial(self):
+        return {'name': self.partner.name}
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
-        ctx['queryset_description'] = self.description
-        ctx['form'] = self.form
-        ctx['is_filtered'] = self.is_filtered
+        ctx['partner'] = self.partner
+        ctx['title'] = self.partner.name
+        ctx['users'] = self.partner.users.all()
         return ctx
+
+    def form_valid(self, form):
+        messages.success(
+            self.request, _("Partner '%s' was updated successfully.") %
+            self.partner.name)
+        self.partner.name = form.cleaned_data['name']
+        self.partner.save()
+        return super().form_valid(form)
+    
+    def get_queryset(self):
+        """
+        Build the queryset for this list
+        """
+        queryset = Product.objects.browsable_dashboard().base_queryset()
+        queryset = self.filter_queryset(queryset)
+        queryset = self.apply_search(queryset)
+        return queryset
+        print(queryset)
