fa0ce72f2410b47829d3906fb1ccae4d0ffbbaa3
Fatma Gaye LO <FGLO>
2021-04-23 12:08:01 +0000

    boutique link to details

--- boutique/views.py
+++ boutique/views.py
@@ -21,7 +21,7 @@ class BoutiqueListView(generic.ListView):
         qs = self.model._default_manager.all()
         qs = sort_queryset(qs, self.request, ['name'])
 
-        self.description = ("All partners")
+        self.description = ("All boutiques")
 
         # We track whether the queryset is filtered to determine whether we
         # show the search form 'reset' button.
@@ -47,10 +47,38 @@ class BoutiqueListView(generic.ListView):
         return ctx
 
 
-    # model = Boutique
-    # template_name = 'boutique/boutique_list.html'
-    # context_object_name = 'boutique_list'
+
 class BoutiqueDetailView(generic.DetailView):
-    model = Boutique
+    model = Partner
     template_name = 'boutique/boutique_details.html'
-    context_object_name = 'boutique'
+    context_object_name = 'partners'
+    form_class = PartnerSearchForm
+    #success_url = reverse_lazy('dashboard:partner-list')
+    def get_queryset(self):
+        qs = self.model._default_manager.all()
+        qs = sort_queryset(qs, self.request, ['name'])
+
+        self.description = ("All boutiques")
+
+        # We track whether the queryset is filtered to determine whether we
+        # show the search form 'reset' button.
+        self.is_filtered = False
+        self.form = self.form_class(self.request.GET)
+        if not self.form.is_valid():
+            return qs
+
+        data = self.form.cleaned_data
+
+        if data['name']:
+            qs = qs.filter(name__icontains=data['name'])
+            self.description = _("Partners matching '%s'") % data['name']
+            self.is_filtered = True
+
+        return qs
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['queryset_description'] = self.description
+        ctx['form'] = self.form
+        ctx['is_filtered'] = self.is_filtered
+        return ctx
